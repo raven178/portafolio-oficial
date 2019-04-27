ece65d9bf1b2fd1863f7c88c95a4f3d8676f4763
Jose Manuel <crashover92@gmail.com>
2019-04-27 11:41:54 -0500

    Se construyo la página principal navbar y footer y rutas home y contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//componentes
+import { HomeComponent } from './components/pages/home/home.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path:'home',component: HomeComponent },
+	{ path:'contacto',component: ContactComponent },
+	{ path:'**', pathMatch: 'full',redirectTo: 'home' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
